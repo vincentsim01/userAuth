fe8c654c566ce88be4a127174fa337b2cf9f1a3b
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-12-20 21:50:10 +0800

    just add the code first

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+// const db = require('./db.js');
+const dotenv = require('dotenv');
+dotenv.config();
+const port = 5001;
+const AuthController = require('./controller/authController')
+
+app.use(cors());
+app.use('/api/auth', AuthController);
+
+app.listen(port,() => {
+    console.log(`Running on port ${port}`)
+})
+
+app.get('/',(req,res) => {
+    res.send("Hiii From Express")
+})
