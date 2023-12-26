91f5d8603c49f9e574f5d4997573bb80f98376e0
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-12-26 19:39:45 +0800

    ok

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/octnode');
+mongoose.connect('mongodb://localhost:27017/octnode');
 
-mongoose.connect('mongodb://localhost:27017/octnode', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB:', err.message);
-});
+// mongoose.connect('mongodb://localhost:27017/octnode', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => {
+//   console.log('Connected to MongoDB');
+// })
+// .catch((err) => {
+//   console.error('Error connecting to MongoDB:', err.message);
+// });
 
 
 
